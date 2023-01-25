4cfb6d225e511f77227fe5fcc4824b9b17db2914
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-01-25 22:55:52 +0700

    Create variables for server configuration

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./api/routes');
 
+const port = process.env.port || 9000;
+const host = '127.0.0.1';
+
 const init = async () => {
 	const server = Hapi.server({
-		port: 9000,
-		host: 'localhost',
+		port: port,
+		host: host,
 	});
 
 	server.route(routes);
